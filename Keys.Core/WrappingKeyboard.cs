53d9c5583e0be967f6f686170b672d2ff2ccdd43
Tim Marman <tim@marman.org>
2014-10-11 22:38:27 -0400

    the goods

---
+++ Keys.Core/WrappingKeyboard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keys.Core
+{
+	/// <summary>
+	/// A keyboard implementation that wraps on columns and rows, so you can go from last column to first in a single move
+	/// </summary>
+	public class WrappingKeyboard : Keyboard
+	{
+		protected override Tuple<short, short> FindShortestPath(Position start, Position end)
+		{
+			// get the shortest wrapped distance, and compare it to the internal distance
+			var rw = (start.Row < this.Height / 2)
+				? (short)(end.Row - this.Height - start.Row)
+				: (short)(this.Height - start.Row - end.Row);
+			var row = (Math.Abs(rw) < Math.Abs((short)(end.Row - start.Row))) ? rw : (short)(end.Row - start.Row);
+
+			var cw = (start.Column < this.Width/2) 
+				? (short)(end.Column - this.Width - start.Column )
+				: (short)(this.Width - start.Column + end.Column );
+			var col = (Math.Abs(cw) < Math.Abs((short)(end.Column - start.Column))) ? cw : (short)(end.Column - start.Column);
+
+			return new Tuple<short, short>(row, col);
+		}
+	}
+}
---
+++ Keys.Tests/Tests.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Keys.Core;
+using NUnit.Framework;
+
+namespace Keys.Tests
+{
+	[TestFixture]
+	public class Tests
+	{
+		[SetUp]
+		public void Setup()
+		{
+
+		}
+
+		[Test]
+		public void TestBasicInput()
+		{
+			var input = "IT Crowd";
+			// Sample output in the problem set is wrong, missing comma after a couple of commands
+			// var output	= "D,R,R,#,D,D,L,#,S,U,U,U,R,#,D,D,R,R,R,#L,L,L#,D,R,R,#,U,U,U,L#";
+			var output		= "D,R,R,#,D,D,L,#,S,U,U,U,R,#,D,D,R,R,R,#,L,L,L,#,D,R,R,#,U,U,U,L,#";
+
+			var kb = new Keyboard();
+			var result = kb.GenerateCommands(input);
+			Assert.AreEqual(output, result);
+
+			input = "Cranberry Beret";
+			output = "R,R,#,D,D,R,R,R,#,U,U,L,L,L,L,L,#,D,D,R,#,U,U,#,R,R,R,#,D,D,R,#,#,D,D,L,L,L,L,L,#,S,U,U,U,U,R,#,R,R,R,#,D,D,R,#,U,U,L,#,D,D,D,L,L,L,#";
+			kb = new Keyboard();
+			result = kb.GenerateCommands(input);
+			Assert.AreEqual(output, result);
+
+			input = "Tru3 F1t";
+			output = "D,D,D,R,#,U,R,R,R,R,#,D,L,L,L,#,D,R,R,#,S,U,U,U,U,R,#,D,D,D,D,L,L,L,#,U,L,#";
+			kb = new Keyboard();
+			result = kb.GenerateCommands(input);
+			Assert.AreEqual(output, result);
+		}
+
+		[Test]
+		public void TestFileInput()
+		{
+			var kb = new Keyboard();
+			var output = "D,R,R,#,D,D,L,#,S,U,U,U,R,#,D,D,R,R,R,#,L,L,L,#,D,R,R,#,U,U,U,L,#";
+			var cmds = kb.ProcessFile(@"App_Data\ITCrowd.txt");
+			foreach (var cmd in cmds)
+				Assert.AreEqual(output, cmd);
+		}
+
+		[Test]
+		public void TestCursorReset()
+		{
+			var input = "IT Crowd";
+			var output = "D,R,R,#,D,D,L,#,S,U,U,U,R,#,D,D,R,R,R,#,L,L,L,#,D,R,R,#,U,U,U,L,#";
+
+			var kb = new Keyboard();
+			var result1 = kb.GenerateCommands(input);
+			Assert.AreEqual(output, result1);
+
+			kb.ResetCursor();
+			var result2 = kb.GenerateCommands(input);
+			Assert.AreEqual(result1, result2);
+
+			var result3 = kb.GenerateCommands(input);
+			Assert.AreNotEqual(result2, result3);
+		}
+
+		[Test]
+		public void TestEdges()
+		{
+			var kb = new Keyboard();
+			var input = "AF05";
+			var output = "#,R,R,R,R,R,#,D,D,D,D,D,#,L,L,L,L,L,#";
+			var result = kb.GenerateCommands(input);
+			Assert.AreEqual(output, result);
+
+		}
+
+		[Test]
+		public void TestWrappingKeyboard()
+		{
+			var input = "IT Crowd";
+			var output = "D,R,R,#,D,D,L,#,S,U,U,U,R,#,D,D,R,R,R,#,L,L,L,#,D,R,R,#,U,U,U,L,#";
+			var kb = new WrappingKeyboard();
+			var result = kb.GenerateCommands(input);
+			Assert.AreEqual(output, result);
+
+			input = "AF05";
+			output = "#,L,#,U,#,R,#";
+			kb.ResetCursor();
+			result = kb.GenerateCommands(input);
+			Assert.AreEqual(output, result);
+
+			input = "BF9A";
+			output = "R,#,L,L,#,U,L,#,D,R,R,#";
+			kb.ResetCursor();
+			result = kb.GenerateCommands(input);
+			Assert.AreEqual(output, result);
+		}
+
+		[Test]
+		public void TestLayouts()
+		{
+			var input = "IT Crowd";
+			var output = "D,D,#,D,D,R,R,R,#,S,U,U,U,U,L,#,D,D,D,D,L,#,U,R,#,D,D,#,U,U,U,U,U,R,#";
+			var kb = new Keyboard(width: 4);
+			var result1 = kb.GenerateCommands(input);
+			Assert.AreEqual(output, result1);
+
+			var kb2 = new Keyboard();
+			var result2 = kb2.GenerateCommands(input);
+			Assert.AreNotEqual(result1, result2);
+		}
+
+		[Test]
+		public void TestConsoleApp()
+		{
+			var files = new List<string> {@"App_Data\ITCrowd.txt"};
+			Console.Program.Main(files.ToArray());
+
+			files.Add(@"App_Data\SomeOtherFile.txt");
+			Console.Program.Main(files.ToArray());
+
+			files.Add(@"App_Data\SomeBadFile.txt");
+			Console.Program.Main(files.ToArray());
+		}
+	}
+}
