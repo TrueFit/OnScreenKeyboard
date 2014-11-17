ae84f329f3f8eaf745b995f636b9f938d13e8f7c
Oliver Spryn <wot200@gmail.com>
2014-11-17 05:46:41 +0000

    Adding input file and constats file

--- src/lib.c
+++ src/lib.c
@@ -1,4 +1,3 @@
-#include "../header/constants.h"
 #include "../header/lib.h"
 
 ///
@@ -22,7 +21,7 @@ void openFile(FILE **fp, int argc, char **argv) {
 	
 //Was any input file given?
 	if(argc == 1) {
-		fprintf(stderr, "An input file was not specified.\n");
+		fprintf(stderr, LIB_NO_INPUT_FILE);
 		promptForFile(file);
 	} else {
 		strncpy(file, argv[1], sizeof(file));
@@ -31,10 +30,13 @@ void openFile(FILE **fp, int argc, char **argv) {
 //Does this file exist?
 	do {
 		if((*fp = fopen(file, "r")) == NULL) {
-			fprintf(stderr, "The specified file does not exist or is not accessable.\n");
+			fprintf(stderr, LIB_FILE_OPENING_ERROR);
 			promptForFile(file);
 		}
 	} while(*fp == NULL);
+	
+//Minor output formatting adjustment
+	if(argc == 1) printf("\n");
 }
 
 /// <summary>
@@ -49,7 +51,7 @@ void promptForFile(char *buffer) {
 	char path[256];
 	
 	do {
-		printf("Please enter the input file path here: ");
+		printf(LIB_ENTER_FILE);
 		fgets(path, sizeof(path), stdin);
 	} while(strlen(path) == 1); // 1 for new line
 	
@@ -64,9 +66,82 @@ void promptForFile(char *buffer) {
 }
 
 ///
-/// Parsing
+/// Parsing and calculations
 /// ----------------------------------------
 ///
+
+/// <summary>
+/// Outputs a string of characters which correspond to the keyboard 
+/// movement which is required to output a given string. This method
+/// takes in an array of ASCII codes, corresponding to an input string,
+/// and uses those values to perform a constant-time calculation to
+/// output the string of characters which correspond to the input
+/// movement.
+/// </summary>
+///
+/// <param name="ascii">An array of ASCII character corresponding to a user's input string</param>
+/// <param name="length">The length of the input array</param>
+/// <param name="outStream">The output stream to which the keyboard movement strokes should be printed</param>
+void calculatePosition(int *ascii, int length, FILE *outStream) {
+	int code = 0;
+	Position cur;
+	int i = 0;
+	Position prev;
+	prev.x = 0;
+	prev.y = 0;
+	
+//Does this line actually contain anything?
+	if(length == 0) {
+		fprintf(outStream, "%s\n", LIB_NO_CHARACTERS);
+		return;
+	}
+	
+	for(i = 0; i < length; ++i) {
+	//Convert A - Z to the correct position in the keyboard matrix
+		if(ascii[i] >= 65 && ascii[i] <= 90) {
+			if(i != 0) fprintf(outStream, ",");
+			
+			code = ascii[i] - 65; // Align the ASCII codes to the keyboard matrix
+			cur.x = code % X;     // Column
+			cur.y = code / Y;     // Row
+	//Convert 0 - 9 to the correct position in the keyboard matrix
+		} else if (ascii[i] >= 48 && ascii[i] <= 57) {
+			if(i != 0) fprintf(outStream, ",");
+			
+			code = ascii[i] - 48 + 26; // Align the ASCII codes to the keyboard matrix
+			cur.x = code % X;          // Column
+			cur.y = code / Y;          // Row
+	//Take into account a space
+		} else if (ascii[i] == 32) {
+			if(i != 0) fprintf(outStream, ",");
+			fprintf(outStream, "S");
+			continue;
+	//Ignore all characters not on the keyboard
+		} else {
+			continue;
+		}
+		
+	//Calculate the offset of the new character from that of the previous character
+	//Up or down
+		if((cur.y - prev.y) < 0) {
+			fprintf(outStream, "%s", up[abs(cur.y - prev.y) - 1]);     // -1 aligns to 0-based array
+		} else if((cur.y - prev.y) > 0) {
+			fprintf(outStream, "%s", down[(cur.y - prev.y) - 1]);
+		}
+	
+	//Left or right
+		if((cur.x - prev.x) < 0) {
+			fprintf(outStream, "%s", left[abs(cur.x - prev.x) - 1]);
+		} else if((cur.x - prev.x) > 0) {
+			fprintf(outStream, "%s", right[(cur.x - prev.x) - 1]);
+		}
+		
+		prev = cur;
+		fprintf(outStream, "#");
+	}
+	
+	fprintf(outStream, "\n");
+}
 
 /// <summary>
 /// Read in a line from the input file, and return an array of numbers,
@@ -93,12 +168,13 @@ void lineToASCII(char *line, int *ascii, int *length) {
 		
 	//Output any warnings
 		if (c != line[i]) {
-			fprintf(stderr, "Warning: \"%c\" was converted to uppercase.\n", line[i]);
+			fprintf(stderr, LIB_WARNING_UPPERCASE, line[i]);
 		} else if (c == '\n' || c == '\r') {
 			//New line, we're at the end
+			--(*length);
 			return;
 		} else if (!(code == 32 || (code >= 48 && code <= 57) || (code >= 65 && code <= 90))) {
-			fprintf(stderr, "Warning: \"%c\" is not present on the keyboard, ignoring.", c);
+			fprintf(stderr, LIB_WARNING_CHARACTER_NOT_PRESENT, c);
 			
 			code = -1;
 		}
