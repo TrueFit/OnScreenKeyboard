c001453f0d0eec8702543a15e080b5ee6a221c42
DFreds <dfreds08@gmail.com>
2016-07-28 14:28:56 -0400

    Added unit tests for backend service

---
+++ OnScreenKeyboard.Tests/Controllers/api/OnScreenKeyboardControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnScreenKeyboard.Controllers.api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnScreenKeyboard.Tests.Controllers.api
+{
+    [TestClass]
+    public class OnScreenKeyboardControllerTest
+    {
+        [TestMethod]
+        public void CalculateResults()
+        {
+            OnScreenKeyboardController controller = new OnScreenKeyboardController();
+
+        }
+    }
+}
