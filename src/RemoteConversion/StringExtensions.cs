304cb7ddfc18d0e2e68a4f3c6e3ac20e1d76c6d0
xorscode <joshua.hollis@gmail.com>
2016-08-08 23:31:49 -0400

    Initial Commit of trufit coding exercise

---
+++ src/RemoteConversion/StringExtensions.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StringExtensions
+{
+    public static class Converters
+    {
+        #region Public String Extension Method: ToDirections
+        /// <summary>
+        /// Converts a string of letters and numbers into a string of directions on a 6 x 6 grid. 
+        /// </summary>
+        /// <param name="str">String</param>
+        /// <returns>String</returns>
+        public static string ToDirections(this string s)
+        {
+            string result = "";
+            GridLocation loc = new GridLocation() { X = 0, Y = 0 }; // x and y coordintes of the current location on the grid.
+
+            foreach(char c in s)
+            {
+                if (c == 32) result += "S"; // add S character if c is a space.
+                else
+                {
+                    int b = AlignCharToGrid(c); // align the input character to conform to our 6x6 transform grid.
+
+                    // if the character is within acceptible parameters, translate it to grid directions.
+                    if (b >= 0 && b <= 36)
+                    {
+                        // calculate the column and row of the character.
+                        int col = (int)Math.Floor((decimal)b % 6);
+                        int row = (int)Math.Floor((decimal)b / 6);
+
+                        // calculate the distance between the current grid location and the location of our desired character.
+                        int distanceX = col - loc.X, distanceY = row - loc.Y;
+
+                        if (distanceX == 0 && distanceY == 0) result += "#";
+                        else
+                        {
+                            result += new String((distanceX > 0) ? 'R' : 'L', Math.Abs(distanceX));
+                            result += new String((distanceY > 0) ? 'D' : 'U', Math.Abs(distanceY));
+
+                            result += "#";
+
+                            loc.X = col;
+                            loc.Y = row;
+                        }
+
+                    }
+                    else throw new Exception();
+                }
+            }
+            
+            return result.Delimit(','); // delimit the result.
+        }
+
+        private class GridLocation
+        {
+            public int X;
+            public int Y;
+        }
+
+        private static int AlignCharToGrid(int c)
+        {
+            int b = (c == 48) ? 100 : c;
+            b = b + ((b >= 49 && b <= 57) ? 41 : 0);
+            b = b - 65;
+
+            return b;
+        }
+        #endregion
+
+        #region Public String Extension Method: Delimit
+        /// <summary>
+        /// Insert the character specified by the delimiter at every other character location in the string, effectively alternating the contents of the string with the delimiter.
+        /// </summary>
+        /// <param name="s">string</param>
+        /// <param name="delimiter">char</param>
+        /// <returns>string</returns>
+        public static string Delimit(this string s, char delimiter)
+        {
+            string result = "";
+            int count = 0;
+            foreach(char c in s)
+            {
+                result += c;
+                if (count < s.Count()-1) result += delimiter;
+                count++;
+            }
+            return result;
+        }
+        #endregion
+
+    }
+}
---
+++ src/RemoteConversionTest_x86-64/RemoteConversionTest_x86-64/Program.cs
@@ -0,0 +1,52 @@
+﻿using StringExtensions;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace RemoteConversionTest_x86_64
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] lines;
+
+            string executableLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            // attempt to load the file specified in args[0]
+            try
+            {
+                lines = File.ReadAllLines(Path.Combine(executableLocation, args[0]));
+            }
+            catch (FieldAccessException)
+            {
+                Console.WriteLine("File not found.");
+                System.Console.ReadKey();
+                return;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Please specify a file to translate.");
+                System.Console.ReadKey();
+                return;
+            }
+
+            foreach (string line in lines)
+            {
+                // attempt to translate the current line to a string of directions and output the result to the console.
+                try
+                {
+                    Console.WriteLine(string.Format("{0} - {1}", line, line.ToDirections())); // output the original line, then the translated line.
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine(string.Format("{0} - Contains characters that cannot be translated.", line));
+                    continue;
+                }
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
