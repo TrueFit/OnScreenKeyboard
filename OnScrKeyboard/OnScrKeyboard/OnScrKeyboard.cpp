02cf77cc0a6504be0e65b852fce018ffb8125b1b
Kevin Bender <kevin.bender13@gmail.com>
2016-09-05 19:20:13 -0400

    initial commit
    inital project setup

---
+++ OnScrKeyboard/OnScrKeyboard/OnScrKeyboard.cpp
@@ -0,0 +1,20 @@
+/**
+* Purpose: The on screen keyboard application reads search terms from a user specified input file, and returns the 
+* cursor path for each input term on the console.
+* 
+* Author: kbender
+*/
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	// request the input file from the user
+    return 0;
+	// read the search terms from the file...
+
+
+}
+
