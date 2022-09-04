6a0ae8c9eb6e10354fd98744696c61d0ce728812
Owen Alexander <60371379+oalexander-dev@users.noreply.github.com>
2022-09-04 00:20:14 -0400

    Implemented keyboard component functionality and tests

---
+++ src/keyboard/keyboard.test.ts
@@ -0,0 +1,53 @@
+/**
+ * src/keyboard/keyboard.test.ts
+ * 
+ * Unit tests for the keyboard component
+ */
+
+import Keyboard from './keyboard';
+
+
+const layout = [
+    ['A', 'B', 'C', 'D', 'E', 'F'],
+    ['G', 'H', 'I', 'J', 'K', 'L'],
+    ['M', 'N', 'O', 'P', 'Q', 'R'],
+    ['S', 'T', 'U', 'V', 'W', 'X'],
+    ['Y', 'Z', '1', '2', '3', '4'],
+    ['5', '6', '7', '8', '9', '0']
+];
+
+describe('testing keyboard class', () => {
+    const kb = new Keyboard(layout);
+
+    // typical cases
+    test('should return correct position for typical alphabetic values', () => {
+        const pos = kb.getKeyPosition('I');
+        expect(pos?.row).toBe(1);
+        expect(pos?.col).toBe(2);
+    });
+
+    test('should return correct position for typical numeric values', () => {
+        const pos = kb.getKeyPosition('2');
+        expect(pos?.row).toBe(4);
+        expect(pos?.col).toBe(3);
+    });
+
+    // edge cases
+    test('should return correct position for corner values', () => {
+        let pos = kb.getKeyPosition('A');
+        expect(pos?.row).toBe(0);
+        expect(pos?.col).toBe(0);
+
+        pos = kb.getKeyPosition('F');
+        expect(pos?.row).toBe(0);
+        expect(pos?.col).toBe(5);
+
+        pos = kb.getKeyPosition('5');
+        expect(pos?.row).toBe(5);
+        expect(pos?.col).toBe(0);
+
+        pos = kb.getKeyPosition('0');
+        expect(pos?.row).toBe(5);
+        expect(pos?.col).toBe(5);
+    });
+});
---
+++ src/keyboard/keyboard.ts
@@ -0,0 +1,49 @@
+/**
+ * src/keyboard/keyboard.ts
+ * 
+ * Class that represents the keyboard component of the
+ * system
+ */
+
+export type Position = {
+    row: number;
+    col: number;
+};
+
+class Keyboard {
+    // mappings from a key value to a position
+    private keys: Map<string, Position>;
+
+    constructor(layout: string[][]) {
+        this.keys = new Map<string, Position>();
+
+        // loop over layout and add each keys position
+        layout.forEach((row, rowIndex) => {
+            row.forEach((val, colIndex) => {
+                // check for a duplicate key value
+                if (this.keys.has(val)) {
+                    console.warn("Duplicate key detected in layout: " + val);
+                }
+
+                // insert the key
+                this.keys.set(val, { row: rowIndex, col: colIndex });
+            });
+        });
+    }
+
+    getKeyPosition(val: string) : Position | null {
+        const position = this.keys.get(val);
+
+        // if the key was found, return its position
+        if (position) {
+            return position;
+        }
+
+        // handle a missing key
+        console.warn("Key not found: " + val);
+        return null;
+    }
+}
+
+
+export default Keyboard;
