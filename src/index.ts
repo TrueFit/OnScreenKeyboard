920214f052a6b464ecf2b8d9d7d76b177d516fc1
Owen Alexander <60371379+oalexander-dev@users.noreply.github.com>
2022-09-04 14:49:41 -0400

    Switched over to promise usage in main driver

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,45 @@
  * streams and initializes path finder
  */
 
- import * as readline from 'node:readline';
+import { Readable, Writable } from 'node:stream';
+import { createReadStream } from 'node:fs';
+
+import PathFinder from './path-finder';
+import { exit } from 'node:process';
 
 
- // initialize an IO interface with desired data source and write location
- const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
- });
- 
+// initialize the input stream to get search terms from, this
+// can be any stream that implements Readable
+const istream: Readable = createReadStream('input.txt');
+// const istream: Readable = process.stdin;
+
+// handle any errors with opening input stream
+istream.on('error', (err) => {
+   console.error(err);
+   exit(1);
+});
+
+// initialize the output stream to print resulting cursor paths to
+const ostream: Writable = process.stdout;
+
+// handle any errors with opening output stream
+ostream.on('error', (err) => {
+   console.error(err);
+   exit(1);
+});
+
+// define the layout of the keyboard
+const layout = [
+   ['A', 'B', 'C', 'D', 'E', 'F'],
+   ['G', 'H', 'I', 'J', 'K', 'L'],
+   ['M', 'N', 'O', 'P', 'Q', 'R'],
+   ['S', 'T', 'U', 'V', 'W', 'X'],
+   ['Y', 'Z', '1', '2', '3', '4'],
+   ['5', '6', '7', '8', '9', '0']
+];
+
+// initialize and run the path finder
+const pathFinder = new PathFinder(layout, istream, ostream);
+pathFinder.computeCursorPaths().then(() => {
+   exit(0);
+});
