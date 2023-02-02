c2cde2ae3875c5cdbe4cb9a9f22a7c2e5a19e2a8
cliffordbateman <cliffbatemandev@gmail.com>
2023-02-01 20:37:31 -0500

    Did vs miss this again

---
+++ OnScreenKeyboard/OnScreenKeyboard.Application/Business/Keyboard/Requests/GetDirectionFromInputRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FluentValidation;
+
+namespace OnScreenKeyboard.Application.Business.Keyboard.Requests
+{
+    //Depending on use case I could do regex checks here if needed, but with a dynamic keyboard object
+    //it's probably better off doing it in the mediatr request
+    //This will fire off as soon as the request object is initialized in the controller
+    public class GetDirectionFromInputRequestValidator : AbstractValidator<GetDirectionFromInputRequest>
+    {
+        public GetDirectionFromInputRequestValidator()
+        {
+            RuleFor(x => x.FileContents)
+                .NotEmpty().WithMessage("File contents are required");
+
+        }
+    }
+}
+
--- OnScreenKeyboard/OnScreenKeyboard.Application/ConfigureApplicationServices.cs
+++ OnScreenKeyboard/OnScreenKeyboard.Application/ConfigureApplicationServices.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.DependencyInjection;
 using OnScreenKeyboard.Application.Common.Behaviors;
 using System.Reflection;
 using OnScreenKeyboard.Application.Services.Keyboard;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -11,6 +13,8 @@ namespace Microsoft.Extensions.DependencyInjection
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
+            services.AddFluentValidationAutoValidation();
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
             services.AddScoped<IKeyboard, StandardKeyboard>();
